d8685c021b47a6049bf30574a0a158b80b33f14a
suke <mwbspa@gmail.com>
2021-02-28 15:46:43 +0900

    Created model Weather_api

---
+++ db/migrate/20210228054405_create_weather_apis.rb
@@ -0,0 +1,12 @@
+class CreateWeatherApis < ActiveRecord::Migration[6.1]
+  def change
+    create_table :weather_apis do |t|
+      t.date :dated_on, null: false
+      t.string :weather, null: false
+      t.string :weather_icon, null: false
+      t.float :temperature, null: false
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/weather_apis.rb
@@ -0,0 +1,4 @@
+FactoryBot.define do
+  factory :weather_api do
+  end
+end
