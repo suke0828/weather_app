b665f6832edbc783dcc997ea7d1c89b1d8c0c93d
suke <mwbspa@gmail.com>
2021-05-04 14:26:38 +0900

    ローカルでcodecovを走らせないように修正 #110

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,17 @@
 require 'simplecov'
 
-require 'codecov'
-SimpleCov.formatter = SimpleCov::Formatter::Codecov
-
 if ENV['CIRCLE_ARTIFACTS']
   dir = File.join(ENV['CIRCLE_ARTIFACTS'], 'coverage')
   SimpleCov.coverage_dir(dir)
 end
 
 SimpleCov.start
+
+# ローカルでテスト走らせない
+if ENV['CIRCLE_ARTIFACTS']
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
